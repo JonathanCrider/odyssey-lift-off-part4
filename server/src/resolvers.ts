6f6b999003d45e1eef11149e60f5d16dea9388ad
JonathanCrider <13684007+JonathanCrider@users.noreply.github.com>
2025-03-28 18:02:33 -0400

    [add] api method and mutation resolver

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,29 +1,50 @@
-import { Resolvers } from "./types";
+import { Resolvers } from './types'
 
 export const resolvers: Resolvers = {
   Query: {
     // returns an array of Tracks that will be used to populate the homepage grid of our web client
     tracksForHome: (_, __, { dataSources }) => {
-      return dataSources.trackAPI.getTracksForHome();
+      return dataSources.trackAPI.getTracksForHome()
     },
 
     // get a single track by ID, for the track page
     track: (_, { id }, { dataSources }) => {
-      return dataSources.trackAPI.getTrack(id);
+      return dataSources.trackAPI.getTrack(id)
     },
 
     // get a single module by ID, for the module detail page
     module: (_, { id }, { dataSources }) => {
-      return dataSources.trackAPI.getModule(id);
+      return dataSources.trackAPI.getModule(id)
     },
   },
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
-      return dataSources.trackAPI.getAuthor(authorId);
+      return dataSources.trackAPI.getAuthor(authorId)
     },
 
     modules: ({ id }, _, { dataSources }) => {
-      return dataSources.trackAPI.getTrackModules(id);
-    },
+      return dataSources.trackAPI.getTrackModules(id)
+    }
   },
-};
+
+  Mutation: {
+    incrementTrackViews: async (_, { id }, { dataSources }) => {
+      try {
+        const track = await dataSources.trackAPI.incrementTrackViews(id)
+        return {
+          code: 200,
+          success: true,
+          message: `Successfully incremented number of views for track ${id}`,
+          track
+        }
+      } catch (error) {
+        return {
+          code: error.extensions.response.status,
+          success: false,
+          message: error.extensions.response.body,
+          track: null
+        }
+      }
+    }
+  }
+}
